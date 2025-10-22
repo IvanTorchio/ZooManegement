dbeee84b5eedc0d796d82f19562df9125b17d507
IvanTorchio <torchioivan2008@gmail.com>
2025-10-22 11:28:54 +0200

    Visual Studio project files added

---
+++ TorchioZoo/TorchioZoo/Animale.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorchioZoo
+{
+    class Animale
+    {
+        public string Nome { get; set; }
+        public string Specie { get; set; }
+        public int Eta { get; set; }
+        public int Peso { get; set; }
+        public string StatoDiSalute { get; set; }
+        public DateOnly DataDiArrivo { get; set; }
+        public int AnniAlloZoo { get; set; }
+
+        private double _pesoIniziale;
+        private int _numeroControlliVeterinari;
+
+        public virtual double CalcolaCiboDiarioKg()
+        {
+            return 0;
+        }
+        public virtual double CalcolaCostoGestioneMensile()
+        {
+            return 0;
+        }
+        public virtual string EmettiVerso()
+        {
+            return "";
+        }
+        public void AggiornaPeso(double nuovoPeso)
+        {
+            
+        }
+        public void EffettuaControlloVeterinario()
+        {
+            _numeroControlliVeterinari++;
+        }
+        public virtual string MostraInformazioni()
+        {
+            return "";
+        }
+        private void CalcolaVariazionePesoPercentuale()
+        {
+            
+        }
+        private void DetermnaStatoSaluteAutomatico()
+        {
+
+        }
+    }
+}
---
+++ TorchioZoo/TorchioZoo/Leone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorchioZoo
+{
+    class Leone : Animale
+    {
+        public bool IsMaschio { get; set; }
+        public int LunghezzaCriniera { get; set; }
+
+        private double _temperaturaCorporeaC = 38.5;
+        private DateTime _ultimaPredazione;
+
+        public override double CalcolaCiboDiarioKg()
+        {
+            return 10;
+        }
+        
+    }
+}
